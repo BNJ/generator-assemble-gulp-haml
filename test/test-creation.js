854bdaf3c75e5f60a7aacc80629d98f4ecc2e83a
Alex Luke <alexl@bnj.com>
2014-07-07 16:37:09 -0700

    Test for expected files

--- test/test-creation.js
+++ test/test-creation.js
@@ -19,14 +19,28 @@ describe('assemble-gulp-haml generator', function () {
 
   it('creates expected files', function (done) {
     var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
+      'bower.json',
+      '.bowerrc',
+      'config.rb',
+      '.editorconfig',
+      'Gemfile',
+      '.gitignore',
+      'gulpfile.js',
+      'package.json',
+      '.ruby-gemset',
+      '.ruby-version',
+      'src/fonts',
+      'src/images',
+      'src/stylesheets/modules',
+      'src/stylesheets/pages',
+      'src/stylesheets/theme/_layout.scss',
+      'src/stylesheets/theme/_theme.scss',
+      'src/stylesheets/theme/_typography.scss',
+      'src/templates/layouts/default.haml',
+      'src/templates/pages/index.haml',
+      'src/templates/partials',
     ];
 
-    helpers.mockPrompt(this.app, {
-      'someOption': true
-    });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
       helpers.assertFile(expected);
